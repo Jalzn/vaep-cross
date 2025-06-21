9bf8f5b179ea259169ecfb35630e1fd3988050dc
Jalzn <jalmirfsjr@gmail.com>
2025-06-21 16:39:09 -0300

    implementa a pipeline para criacao das features de tracking

--- pipeline.py
+++ pipeline.py
@@ -1,37 +1,69 @@
+import re
+from pathlib import Path
+
+import pandas as pd
+from tqdm import tqdm
+
+import tracking.features
 import tracking.process
 import tracking.read
 from utils.events import generate_gamestates, read_actions
 
-actions = read_actions()
 
 
-tracking_df = tracking.read.read_by_match_id(13335)
-tracking_df = tracking.process.process(tracking_df, actions, 13335)
+if not (Path("./data/gamestates.parquet").exists() and Path("./data/actions.parquet").exists()):
+    print("Creating raw actions and gamestates")
+    actions = read_actions()
+    gamestates = generate_gamestates(actions)
+    actions.to_parquet("./data/actions.parquet")
+    gamestates.to_parquet("./data/gamestates.parquet")
+else:
+    print("Loading actions and gamestates")
+    actions = pd.read_parquet("./data/actions.parquet")
+    gamestates = pd.read_parquet("./data/gamestates.parquet")
 
-gamestates = generate_gamestates(actions)
+
+tracking_folder = Path("./data/")
+parquet_pattern = re.compile(r'^\d{5}\.parquet$')
+
+files = [f for f in tracking_folder.iterdir() if f.is_file() and parquet_pattern.match(f.name)]
+
+matches = []
+for file in files:
+    pattern = re.match(r'^(\d{5})\.parquet$', file.name)
+    if pattern:
+        matches.append(int(pattern.group(1)))
+
+print(f"Found {len(matches)} matches")
 
 
-ids = gamestates[gamestates["match_id"] == 13335]["event_id"].tolist()
+for match_id in tqdm(matches):
+    tracking_df = tracking.read.read_by_match_id(match_id)
+    tracking_df = tracking.process.process(tracking_df, actions, match_id)
 
-for id in ids:
-    frame = tracking_df[tracking_df["possession_event_id"] == id]
+    ids = gamestates[gamestates["match_id"] == match_id]["event_id"].tolist()
 
-    event = gamestates[gamestates["event_id"] == id].iloc[0]
-    home_team_id = event["team_id"]
+    for id in ids:
+        frame = tracking_df[tracking_df["possession_event_id"] == id]
 
-    attackers, defenders = tracking.features.count_players_in_box(frame, home_team_id)
+        event = gamestates[gamestates["event_id"] == id].iloc[0]
+        home_team_id = event["team_id"]
 
-    gamestates.loc[gamestates["event_id"] == id, "attackers_in_box" ] = attackers
-    gamestates.loc[gamestates["event_id"] == id, "defenders_in_box"] = defenders
+        attackers, defenders = tracking.features.count_players_in_box(frame, home_team_id)
 
-for id in ids:
-    frame = tracking_df[tracking_df["possession_event_id"] == id]
+        gamestates.loc[gamestates["event_id"] == id, "attackers_in_box" ] = attackers
+        gamestates.loc[gamestates["event_id"] == id, "defenders_in_box"] = defenders
 
-    event = gamestates[gamestates["event_id"] == id].iloc[0]
+    for id in ids:
+        frame = tracking_df[tracking_df["possession_event_id"] == id]
 
-    attackers, defenders = tracking.features.count_players_in_zone(frame, event)
+        event = gamestates[gamestates["event_id"] == id].iloc[0]
 
-    gamestates.loc[gamestates["event_id"] == id, "attackers_in_zone" ] = attackers
-    gamestates.loc[gamestates["event_id"] == id, "defenders_in_zone"] = defenders
+        attackers, defenders = tracking.features.count_players_in_zone(frame, event)
 
-gamestates.head()
+        gamestates.loc[gamestates["event_id"] == id, "attackers_in_zone" ] = attackers
+        gamestates.loc[gamestates["event_id"] == id, "defenders_in_zone"] = defenders
+
+    gamestates.to_parquet("./data/gamestates.parquet")
+
+print("Finished")
